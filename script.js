e10778f3f20489c947f866f2793e223882394a10
Rafał Wójcik <rafi.incognito@o2.pl>
2022-04-23 19:51:10 +0200

    feat: contact section

--- script.js
+++ script.js
@@ -52,14 +52,39 @@ var swiper = new Swiper(".review-slider", {
 },
 breakpoints: {
     640: {
-        slidesPreview: 1,
+        slidesPerView: 1,
     },
     768: {
-        slidesPreview: 2,
+        slidesPerView: 2,
     },
     1024: {
-        slidesPreview: 3,
+        slidesPerView: 3,
     },
     
 },
 });
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+},
+breakpoints: {
+    450: {
+        slidesPerView: 2,
+    },
+    768: {
+        slidesPerView: 3,
+    },
+    991: {
+        slidesPerView: 4,
+    },
+    1200: {
+        slidesPerView: 5,
+    },
+    
+},
+});
+
